f776cf47a548bb72bfca5d2ad8bee13bc37bc56c
mehran <mehransi95@gmail.com>
2022-06-20 14:24:12 +0430

    Change load tester to use asyncio instead of locust

--- auto_tuner/experiments/workload.py
+++ auto_tuner/experiments/workload.py
@@ -1,10 +1,10 @@
-import os
 import time
 import json
 import numpy as np
 import matplotlib.pyplot as plt
 from multiprocessing import Process
-
+import asyncio
+from aiohttp import ClientSession
 import redis
 
 from auto_tuner import AUTO_TUNER_DIRECTORY
@@ -23,20 +23,33 @@ images = np.load(
     f"{AUTO_TUNER_DIRECTORY}/experiments/saved_inputs.npy", allow_pickle=True
 )
 
-i = 1
+j = 1
 for image in images:
-    store.set(f"imagenet-{i}", json.dumps(image))
-    i += 1
-    if i == 100:
+    store.set(f"imagenet-{j}", json.dumps(image))
+    j += 1
+    if j == 100:
         break
 
 del images
 
-# def locust_generator(host, rate):
-#     return os.system(
-#         f"locust -f {BASE_DIR}/auto_tuner/experiments/locustfile.py --headless"
-#         f" --host {host} -u {rate} -r {rate} --run-time 1 --stop-timeout 1"
-#     )
+
+async def predict(url, data, delay):
+    await asyncio.sleep(delay)
+    async with ClientSession() as session:
+        async with session.post(url, data=data["data"]) as response:
+            response = await response.read()
+            return response
+
+
+def generate_load_for_second(url, count):
+    loop = asyncio.get_event_loop()
+    tasks = []
+    data = json.loads(store.get(f"imagenet-{np.random.randint(1, 101)}"))
+    delays = np.cumsum(np.random.exponential(1/count, count))
+    for i in range(count):
+        task = asyncio.ensure_future(predict(url, data, delays[i]))
+        tasks.append(task)
+    loop.run_until_complete(asyncio.wait(tasks))
 
 
 def generate_workload(ip, port):
@@ -46,12 +59,16 @@ def generate_workload(ip, port):
     plt.savefig("load_generator.png", format="png")
     plt.close()
     ip = "192.5.86.160"
-
+    url = f"http://{ip}:{port}/v1/models/resnet:predict"
+    processes = []
     for rate in requests:
-        # locust_generator_process = Process(target=locust_generator, args=(f"http://{ip}:{port}", rate))
-        # locust_generator_process.start()
-        os.popen(
-            f"locust -f {AUTO_TUNER_DIRECTORY}/experiments/locustfile.py --headless"
-            f" --host http://{ip}:{port} -u {rate} -r {rate} --run-time 1 --stop-timeout 2"
-        )
+        generator_process = Process(target=generate_load_for_second, args=(url, rate))
+        generator_process.daemon = True
+        generator_process.start()
+        processes.append(generator_process)
         time.sleep(1)
+        procs = []
+        for p in processes:
+            if p.exitcode is None:
+                procs.append(p)
+        processes = procs
